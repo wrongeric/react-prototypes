ae915ef7c065c281b57ae4493ac1e84020e89e78
wrongeric <33172384+wrongeric@users.noreply.github.com>
2018-01-29 09:58:00 -0800

    added feature set 7

--- src/components/contact_form.js
+++ src/components/contact_form.js
@@ -16,6 +16,7 @@ class ContactForm extends Component {
 
         this.handleInputChange = this.handleInputChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.reset = this.reset.bind(this);
     }
 
     handleInputChange(event) {
@@ -31,6 +32,17 @@ class ContactForm extends Component {
     handleSubmit(event){
         event.preventDefault();
         console.log('handleSubmit called, form values are: ', this.state.form);
+        this.reset();
+    }
+    reset(){
+        this.setState({
+            form: {
+                firstName: '',
+                lastName: '',
+                phone: '',
+                email: '',
+            }
+        });
     }
     render(){
         const { firstName, lastName, phone, email } = this.state.form;
@@ -41,8 +53,8 @@ class ContactForm extends Component {
                 <Field name="lastName" label="Last Name" type="text" value={lastName} onChange={this.handleInputChange}/>
                 <Field name="phone" label="Phone Number" type="tel" value={phone} onChange={this.handleInputChange}/>
                 <Field name="email" label="Email" type="email" value={email} onChange={this.handleInputChange}/>
-
                 <button>Add Contact</button>
+                <button type='button' onClick={this.reset}>Reset</button>
             </form>
         )
     }
