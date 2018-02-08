294402037a7f02e6b5d72cbe1d6c5abcba359ee1
wrongeric <33172384+wrongeric@users.noreply.github.com>
2018-02-08 14:00:10 -0800

    pushing changes before changing branches

--- src/components/app.js
+++ src/components/app.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route} from 'react-router-dom';
+import {BrowserRouter as Router, NavLink, Route} from 'react-router-dom';
 import Welcome from './welcome';
 import Nav from './nav';
 import OurMacarons from './our_macarons';
@@ -10,13 +10,15 @@ import Contact from './contact';
 
 const App = () => {
     return (
-        <div className="container">
+        <Router>
+            <div className="container">
             <Nav/>
             <Route exact path="/" component={Welcome}></Route>
             <Route path="/our_macarons" component={OurMacarons}></Route>
             <Route path="/gifts_parties" component={GiftsParties}></Route>
             <Route path="/contact" component={Contact}></Route>
         </div>
+        </Router>
     )
 };
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app';
-import { BrowserRouter as Router } from 'react-router-dom';
-
 
 ReactDOM.render(
-    <Router>
-        <App/>
-    </Router>,
+    <App/>,
     document.getElementById('root')
 );
